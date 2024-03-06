2ecfdffc259a8c93e29da88424b42c081fc4fba3
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-03-06 13:36:24 -0300

    feat: Adiciona meu primeiro componente

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/vamos">Bora Biu</RouterLink>
+
       </nav>
     </div>
   </header>
