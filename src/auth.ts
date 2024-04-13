b1e513fc327ed95d008a5de15b7fdbf18036d0b8
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-04-13 14:21:30 -0300

    feat: Configura login, logout e tratamento de erros com messagem de erro adequada

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,67 @@
-async function signIn(email: string , password: string, onSuccess: () => void) {
-    console.log("will sign in...")
-    const body = {
-    login: {
-    email: email,
-    password: password
-    }
-    }
+import { storage } from './storage.ts'
+console.log('will sign in...')
 
-    const response = await fetch(
-    "http://localhost:3000/sign_in", {
-    method: "POST",
+function success(response, onSuccess) {
+  response.json().then((json) => {
+    storage.store('token', json.token)
+    storage.store('email', json.email)
+    onSuccess()
+  })
+}
+
+function failure(response, onFailure) {
+  onFailure()
+}
+
+function isloggedIn() {
+  return Boolean(storage.get('token'))
+}
+
+function signOut(andThen = null) {
+  storage.remove('token')
+  storage.remove('email')
+
+  if (typeof andThen == 'function') {
+    andThen()
+  }
+}
+
+function currentUser() {
+  if (!isloggedIn()) {
+    return null
+  }
+  return {
+    email: storage.get('email')
+  }
+}
+
+async function signIn(email, password, onSuccess, onFailure) {
+  const body = {
+    login: {
+      email: email,
+      password: password
+    }
+  }
+
+  const response = await fetch('http://localhost:3000/sign_in', {
+    method: 'POST',
     headers: {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
-    })
-    response.json().then((json) => onSuccess())
+  }).then((response) => {
+    if (response.ok) {
+      success(response, onSuccess)
+    } else {
+      failure(response, onFailure)
     }
+  })
+}
 
-    export const auth = {
-        signIn: signIn
-    }
+export const auth = {
+  signIn,
+  isloggedIn,
+  currentUser,
+  signOut
+}
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,38 +1,46 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import { auth } from '../auth.ts'
+import { auth } from '../auth'
 
 const router = useRouter()
 const email = defineModel('email')
 const password = defineModel('password')
 const awaiting = ref(false)
+const errorMessage = ref('')
 
-function onSubmit(form) {
-awaiting.value = true
-auth.signIn(
-email.value,
-password.value,
-() => {
-awaiting.value = false
-router.push('/')
-})
+async function onSubmit(form) {
+  awaiting.value = true
+  auth.signIn(
+    email.value,
+    password.value,
+    () => {
+      awaiting.value = false
+      router.push('/')
+    },
+    (error) => {
+      awaiting.value = false
+      console.log('não foi dessa vez')
+      errorMessage.value = 'Email ou senha inválido(a)!'
+    }
+  )
 }
 </script>
 
 <template>
-    <div>
+  <div>
     <h1>Sign In</h1>
-    <form @submit.prevent= "onSubmit()">
-    <label>E-Mail: </label>
-    <input v-model="email"
-    type="email"><br />
-    <label>Senha: </label>
-    <input v-model="password"
-    type="password"><br />
-    <button
-    type="submit"
-    v-show="!awaiting">Sign In</button>
+
+    <form @submit.prevent="onSubmit()">
+      <label>E-Mail: </label>
+      <input v-model="email" type="email" /><br />
+
+      <label>Senha: </label>
+      <input v-model="password" type="password" /><br />
+
+      <button type="submit" v-show="!awaiting">Sign In</button>
     </form>
-    </div>
-    </template>
+
+    <div v-if="errorMessage" style="color: red">{{ errorMessage }}</div>
+  </div>
+</template>
