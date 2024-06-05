0a3ec089064f00056108734a064899e63724fcfe
Erikaa81 <erikafabi_oliveira@hotmail.com>
2024-06-05 11:57:15 -0300

    feat: Adiciona componente Registration e cria rota

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignIn from '../components/SignIn.vue'
 import WelcomeView from '../views/WelcomeView.vue'
+import Registration from '../components/Registration.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,9 +16,13 @@ const router = createRouter({
       path: '/signin',
       name: 'signin',
       component: SignIn
+      },
+      {
+        path: '/new',
+        name: 'new',
+        component: Registration
       }
-
-  ]
+    ]  
 })
 
 export default router
